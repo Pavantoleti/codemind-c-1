3bfa6d8a66e86565138c20d766e0ddad5a4c4f1c
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-04-25 16:44:52 +0530

    #include<stdio.h> int rev(int num) {     int sum=0,r;     while (num>0)     {         r=num%10;         sum=(sum*10)+r;         num/=10;     }     return sum; } int main() {     int a;     scanf("%d",&a);     int i,l=0,f=0,dl=0,df=0;     for (i=a-1; i>=0; i--)     {         if (rev(i)==i)         {             f=i;             break;         }     }     for (i=a+1; i<=10000; i++)     {         if (rev(i)==i)         {             l=i;             break;         }     }     df=a-f;     dl=l-a;     if (df==dl)     {         printf("%d %d",f,l);     }     else if (df>dl)     {         printf("%d",l);     }     else     {         printf("%d",f);     }     return 0; }

--- Closest_Palindrome_.c
+++ Closest_Palindrome_.c
@@ -1,54 +1,49 @@
 #include<stdio.h>
+int rev(int num)
+{
+    int sum=0,r;
+    while (num>0)
+    {
+        r=num%10;
+        sum=(sum*10)+r;
+        num/=10;
+    }
+    return sum;
+}
 int main()
 {
-	int n,q1,q2,k1,k2,rem1,rem2,res1,res2,pal1,pal2,l,u;
-	scanf("%d",&n);
-	k1=n-1;
-	for(k1;k1>0;k1--)
-	{
-		q1=k1;
-		res1=0;
-		while(q1>0)
-		{
-			rem1=q1%10;
-			res1=(res1*10)+rem1;
-			q1=q1/10;
-		}
-		if(res1==k1)
-		{
-			pal1=k1;
-			break;
-		}
-	}
-	k2=n+1;
-	for(k2;;k2++)
-	{
-		q2=k2;
-		res2=0;
-		while(q2>0)
-		{
-			rem2=q2%10;
-			res2=(res2*10)+rem2;
-			q2=q2/10;
-		}
-		if(res2==k2)
-		{
-			pal2=k2;
-			break;
-		}
-	}
-    l=n-pal1;
-    u=pal2-n;
-    if(l>u)
+    int a;
+    scanf("%d",&a);
+    int i,l=0,f=0,dl=0,df=0;
+    for (i=a-1; i>=0; i--)
     {
-    	printf("%d",pal2);
-	}
-	else if(l==u)
-	{
-		printf("%d %d",pal1,pal2);
-	}
-	else
-	{
-		printf("%d",pal1);
-	}
+        if (rev(i)==i)
+        {
+            f=i;
+            break;
+        }
+    }
+    for (i=a+1; i<=10000; i++)
+    {
+        if (rev(i)==i)
+        {
+            l=i;
+            break;
+        }
+    }
+    df=a-f;
+    dl=l-a;
+    if (df==dl)
+    {
+        printf("%d %d",f,l);
+    }
+    else if (df>dl)
+    {
+        printf("%d",l);
+    }
+    else
+    {
+        printf("%d",f);
+    }
+    return 0;
 }
