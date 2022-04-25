444d3c2a8d98b9723d1c4fc498b057f4b0b85e4c
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-04-25 16:16:57 +0530

    #include<stdio.h> int sum(int n) {     int su=0,r;     while(n>0)     {         r=n%10;         su+=r;         n=n/10;     }     return su; } int main() {     int n;     scanf("%d",&n);     int m=sum(n);     if(n%m==0)     {         printf("True");     }     else     {         printf("False");     } }

--- Harshed_number.c
+++ Harshed_number.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
-int main()
+int sum(int n)
 {
-    int n,s=0,q,r;
-    scanf("%d",&n);
-    q=n;
+    int su=0,r;
     while(n>0)
     {
         r=n%10;
-        s=s+r;
+        su+=r;
         n=n/10;
     }
-    if(q%s==0)
+    return su;
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int m=sum(n);
+    if(n%m==0)
     {
         printf("True");
     }
