dda316fb4c43cf82df9465fc1fe74e3c2eb80078
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-04-25 17:43:13 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int a;     scanf("%d",&a);     int c=0;     for(i=0;i<n;i++)     {         if(arr[i]==a)         {             c=1;             break;         }     }     if(c==1)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int a;
+    scanf("%d",&a);
+    int c=0;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==a)
+        {
+            c=1;
+            break;
+        }
+    }
+    if(c==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
