e30d3dba8fb5e04569fba7eeb1b74cc1d19ddbd0
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-04-25 16:50:35 +0530

    #include<stdio.h> int prime(int num) {     int c=0,i;     for (i=1; i<=num; i++)     {         if (num%i==0)         {             c++;         }     }     if (c<=2)     {         return 1;     }     return 0; } int main() {     int a;     scanf("%d",&a);     int l,f,dl,df,i;     for (i=a; i>=0; i--)     {         if (prime(i)==1)         {             f=i;             df=a-i;             break;         }     }     for (i=a; i<=10000; i++)     {         if (prime(i)==1)         {             l=i;             dl=i-a;             break;         }     }     if (df==dl)     {         printf("%d",dl);     }     else if (df>dl)     {         printf("%d",dl);     }     else     {         printf("%d",df);     }     return 0; }

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
@@ -1,53 +1,54 @@
 #include<stdio.h>
-#include<math.h>
+int prime(int num)
+{
+    int c=0,i;
+    for (i=1; i<=num; i++)
+    {
+        if (num%i==0)
+        {
+            c++;
+        }
+    }
+    if (c<=2)
+    {
+        return 1;
+    }
+    return 0;
+}
 int main()
 {
-	int n,i,j,up,lp,s,m,l,upd,lpd;
-	scanf("%d",&n);
-	m=n;
-	for(m;;m++)
-	{
-		for(i=2;i<=sqrt(m);i++)
-		{
-			s=1;
-			if(m%i==0)
-			{
-				s=0;
-				break;
-			}
-		}
-		if(s==1)
-		{
-			up=m;
-			break;
-		}
-	}
-	l=n;
-	for(l;l>0;l--)
-	{
-		for(i=2;i<=sqrt(l);i++)
-		{
-			s=1;
-			if(l%i==0)
-			{
-				s=0;
-				break;
-			}
-		}
-		if(s==1)
-		{
-			lp=l;
-			break;
-		}
-	}
-	upd=up-n;
-	lpd=n-lp;
-	if(lpd<=upd)
-	{
-		printf("%d",lpd);
-	}
-	else
-	{
-		printf("%d",upd);
-	}
+    int a;
+    scanf("%d",&a);
+    int l,f,dl,df,i;
+    for (i=a; i>=0; i--)
+    {
+        if (prime(i)==1)
+        {
+            f=i;
+            df=a-i;
+            break;
+        }
+    }
+    for (i=a; i<=10000; i++)
+    {
+        if (prime(i)==1)
+        {
+            l=i;
+            dl=i-a;
+            break;
+        }
+    }
+    if (df==dl)
+    {
+        printf("%d",dl);
+    }
+    else if (df>dl)
+    {
+        printf("%d",dl);
+    }
+    else
+    {
+        printf("%d",df);
+    }
+    return 0;
 }
