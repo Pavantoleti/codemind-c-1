d89068082e76490469d0822f8758d06ff0612f1d
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-04-25 16:38:26 +0530

    #include<stdio.h> #include<math.h> int size(int num) {     int c=0;     while (num>0)     {         c++;         num/=10;     }     return c; } int rev(int num) {     int s=0,r;     while (num>0)     {         r=num%10;         s=(s*10)+r;         num=num/10;     }     return s; } int main() {     int a;     scanf("%d",&a);     int ar=rev(a);     int i,sum=0,c=size(a),j=1;     while (ar>0)     {         int dig=ar%10;         sum+=pow(dig,j);         j++;         ar/=10;     }     if (sum==a)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

--- Disarium_number.c
+++ Disarium_number.c
@@ -1,24 +1,40 @@
 #include<stdio.h>
 #include<math.h>
+int size(int num)
+{
+    int c=0;
+    while (num>0)
+    {
+        c++;
+        num/=10;
+    }
+    return c;
+}
+int rev(int num)
+{
+    int s=0,r;
+    while (num>0)
+    {
+        r=num%10;
+        s=(s*10)+r;
+        num=num/10;
+    }
+    return s;
+}
 int main()
 {
-    int n,sq,c=0,sum=0,temp,r,rev=0;
-    scanf("%d",&n);
-    temp=n;
-    while(temp>0)
+    int a;
+    scanf("%d",&a);
+    int ar=rev(a);
+    int i,sum=0,c=size(a),j=1;
+    while (ar>0)
     {
-        r=temp%10;
-        rev=(rev*10)+r;
-        temp=temp/10;
+        int dig=ar%10;
+        sum+=pow(dig,j);
+        j++;
+        ar/=10;
     }
-    for(int i=1;rev>0;i++)
-    {
-        r=rev%10;
-        sq=pow(r,i);
-        sum = sum+sq;
-        rev=rev/10;
-    }
-    if(sum==n)
+    if (sum==a)
     {
         printf("True");
     }
@@ -26,4 +42,5 @@ int main()
     {
         printf("False");
     }
+    return 0;
 }
