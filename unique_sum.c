cf2e8d8093708fc51f4ef023f6ed6cbeb13956ec
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-04-27 16:33:12 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(int i=0;i<n;i++)     {         for(int j=0;j<n;j++)         {             if(i!=j)             {                 if(arr[i]==arr[j]&&arr[i]!=-1)                 {                     arr[j]=-1;                 }             }         }     }     int sum=0;     for(int i=0;i<n;i++)     {         if(arr[i]!=-1)         {             sum+=arr[i];         }     }     printf("%d",sum); }

---
+++ unique_sum.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                if(arr[i]==arr[j]&&arr[i]!=-1)
+                {
+                    arr[j]=-1;
+                }
+            }
+        }
+    }
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]!=-1)
+        {
+            sum+=arr[i];
+        }
+    }
+    printf("%d",sum);
+}
