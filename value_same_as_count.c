1ef269bb4c605d1cd00b132ec20042814cbeab75
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-04-27 06:28:38 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int arr[a],i,j;     for (i=0; i<a; i++)     {         scanf("%d",&arr[i]);     }     int c=0,count=0;     for (i=0; i<a; i++)     {         count=1;         for (j=0; j<a; j++)         {             if (i!=j)             {                 if (arr[i]==arr[j] && arr[i]!=-1)                 {                     arr[j]=-1;                     count++;                 }             }         }         if (count==arr[i])         {             c++;         }     }     printf("%d",c);     return 0; }

---
+++ value_same_as_count.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a],i,j;
+    for (i=0; i<a; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int c=0,count=0;
+    for (i=0; i<a; i++)
+    {
+        count=1;
+        for (j=0; j<a; j++)
+        {
+            if (i!=j)
+            {
+                if (arr[i]==arr[j] && arr[i]!=-1)
+                {
+                    arr[j]=-1;
+                    count++;
+                }
+            }
+        }
+        if (count==arr[i])
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
