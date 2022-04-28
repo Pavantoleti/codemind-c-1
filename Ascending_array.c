c91921adb540e06bf07f93c4f2759dc333f76893
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-04-28 20:03:56 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int c=0;     for(int i=0;i<n-1;i++)     {         if(arr[i]<arr[i+1])         {             c=1;         }         else         {             c=0;             break;         }     }     if(c==1)     {         printf("yes");     }     else     {         printf("no");     } }

---
+++ Ascending_array.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int c=0;
+    for(int i=0;i<n-1;i++)
+    {
+        if(arr[i]<arr[i+1])
+        {
+            c=1;
+        }
+        else
+        {
+            c=0;
+            break;
+        }
+    }
+    if(c==1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
