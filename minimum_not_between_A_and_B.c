9c8078b1d3ef9c7358d21a83da0f617958757e10
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-04-28 09:46:36 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i;     for (i=0; i<n; i++)     {         scanf("%d",&arr[i]);     }     int a,b,min=10000,c=0;     scanf("%d%d",&a,&b);     for (i=0; i<n; i++)     {         if (arr[i]<a|| arr[i]>b)         {             if (min>arr[i])             {                 min=arr[i];                 c++;             }         }     }     if (c==0)     {         printf("-1");     }     else     {         printf("%d",min);     }     return 0; }

--- minimum_not_between_A_and_B.c
+++ minimum_not_between_A_and_B.c
@@ -1,33 +1,33 @@
 #include<stdio.h>
 int main()
 {
-	int n;
-	scanf("%d",&n);
-	int i,arr[n];
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&arr[i]);
-	}
-	int a,b;
-	scanf("%d%d",&a,&b);
-	int count=0,min=1000;
-	for(i=0;i<n;i++)
-	{
-		if(arr[i]<a || arr[i]>b)
-		{
-			if(arr[i]<min)
-			{
-				min=arr[i];
-			}
-			count++;
-		}
-	}
-	if(count==0)
-	{
-		printf("-1");
-	}
-	else
-	{
-		printf("%d",min);
-	}
+    int n;
+    scanf("%d",&n);
+    int arr[n],i;
+    for (i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int a,b,min=10000,c=0;
+    scanf("%d%d",&a,&b);
+    for (i=0; i<n; i++)
+    {
+        if (arr[i]<a|| arr[i]>b)
+        {
+            if (min>arr[i])
+            {
+                min=arr[i];
+                c++;
+            }
+        }
+    }
+    if (c==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        printf("%d",min);
+    }
+    return 0;
 }
