e0fedea09508cd1d7afe39857f5b8ca1b46603e8
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-04-29 06:40:58 +0530

    #include<stdio.h> int main() {     char str[1000];     scanf("%[^ ]s",str);     int w=1,i;     for (i=0; str[i]!=NULL; i++)     {         if (str[i]==32)         {             w++;         }     }     printf("%d",w);     return 0; }

---
+++ words_in_a_string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char str[1000];
+    scanf("%[^
+]s",str);
+    int w=1,i;
+    for (i=0; str[i]!=NULL; i++)
+    {
+        if (str[i]==32)
+        {
+            w++;
+        }
+    }
+    printf("%d",w);
+    return 0;
+}
