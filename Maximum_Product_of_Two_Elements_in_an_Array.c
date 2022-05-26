e19dff495bb1aabb58e4227ce6fa3bf2891e0fec
arunyarru1729 <97431055+arunyarru1729@users.noreply.github.com>
2022-05-26 08:24:27 +0530

    #include<stdio.h> #include<string.h> int pro(int *arr,int a){     int i,j,max=0,prod;     for (i=0; i<a; i++){         for (j=0; j<a; j++){             if (i!=j){                 prod=(arr[i]-1)*(arr[j]-1);                 if (max<prod){                     max=prod;                 }             }         }     }     return max; } int main(){     char str[1000];     scanf("%[^ ]s",str);     int l=strlen(str),i,i1=0,j=0,k=0;     int arr[100],max=1;     for (i=0; i<l; i++){         if (str[i]==32){             int val=0;             for (j=i1; j<i; j++){                 val=(val*10)+str[j]-48;             }             arr[k++]=val;             i1=i+1;         }         else if (i==l-1){             int val=0;             for (j=i1; j<=i; j++){                 val=(val*10)+str[j]-48;             }             arr[k++]=val;             i1=i+1;             break;         }     }     max=pro(arr,k);     printf("%d",max);     return 0; }

--- Maximum_Product_of_Two_Elements_in_an_Array.c
+++ Maximum_Product_of_Two_Elements_in_an_Array.c
@@ -1,25 +1,45 @@
 #include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    int a[n],i,j,max=0;
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            if(i!=j)
-            {
-                if((a[i]-1)*(a[j]-1)>max)
-                {
-                    max=(a[i]-1)*(a[j]-1);
+#include<string.h>
+int pro(int *arr,int a){
+    int i,j,max=0,prod;
+    for (i=0; i<a; i++){
+        for (j=0; j<a; j++){
+            if (i!=j){
+                prod=(arr[i]-1)*(arr[j]-1);
+                if (max<prod){
+                    max=prod;
                 }
             }
         }
     }
+    return max;
+}
+int main(){
+    char str[1000];
+    scanf("%[^
+]s",str);
+    int l=strlen(str),i,i1=0,j=0,k=0;
+    int arr[100],max=1;
+    for (i=0; i<l; i++){
+        if (str[i]==32){
+            int val=0;
+            for (j=i1; j<i; j++){
+                val=(val*10)+str[j]-48;
+            }
+            arr[k++]=val;
+            i1=i+1;
+        }
+        else if (i==l-1){
+            int val=0;
+            for (j=i1; j<=i; j++){
+                val=(val*10)+str[j]-48;
+            }
+            arr[k++]=val;
+            i1=i+1;
+            break;
+        }
+    }
+    max=pro(arr,k);
     printf("%d",max);
+    return 0;
 }
